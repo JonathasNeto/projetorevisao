13a4068a66e1287bd56bed95a05870054fcd2904
unknown <jonetocrazy@gmail.com>
2020-05-05 14:53:09 -0300

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TurmasService } from './turmas.service';
+import { Aluno } from './models/models.aluno';
 
 @Component({
   selector: 'app-root',
@@ -10,26 +11,56 @@ export class AppComponent implements OnInit{
   title = 'ProjetoRevisao';
   turmas = [];
   turma: any = null;
+  codigo: any = null;
+  aluno: Aluno;
+  frequencia: number;
+  nota1: number;
+  nota2: number;
+  registros = [];
 
-  constructor(private service: TurmasService){
-
-  }
+  constructor(private service: TurmasService){  }
 
   ngOnInit(): void {
     this.service.listar()
     .subscribe((dados: any) => this.turmas = dados); 
   }
   listarAluno(){
-    let lista =[];
-    for(var i in this.turmas){
-      for(var e in this.turmas[i].alunos){
-        lista.push(this.turmas[i].alunos[e]);
-      }
-    }
-    return lista;
-
+      if(this.turma != null)return this.encontrarTurma(this.turma).alunos;
+    ;
   }
-    
-
+    encontrarTurma(parametro){
+    var turm;
+    for(var i in this.turmas){
+      if(this.turmas[i].numero === parametro){
+        turm = this.turmas[i];
+      }
+    }return turm;
+  }
+  salvar() {
+    this.registros.push(
+      { 
+        "nome": this.encontrarAluno(this.aluno,this.turma), 
+        "codigo": this.aluno, 
+        "nota1": this.nota1, 
+        "nota2": this.nota2, 
+        "frequencia": this.frequencia, 
+        "turma": this.turma,
+        "media": this.calcularMedia(this.nota1, this.nota2)
+      });
+  }
  
+  calcularMedia(n1:number, n2:number){
+    return (n1+(n2*2))/3;
+  }
+
+  encontrarAluno(codAluno, codTurma) {
+    var turm = this.encontrarTurma(codTurma).alunos;
+    for (var i in turm) {
+      if (turm[i].codigo == codAluno) {
+        return turm[i].nome;
+      }
+    } 
+    return null;
+  }
+
 }
---
+++ src/app/models/models.aluno.ts
@@ -0,0 +1,5 @@
+export class Aluno{
+    constructor(public nome: String, public nota1: Number, public nota2: Number, public frequencia: Number, public media: Number){
+
+    }
+}
